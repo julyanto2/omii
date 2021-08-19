26fc647a3a5e81acdf9d0f13b82d1116b92d96e3
HackD'mon <88218417+julyanto2@users.noreply.github.com>
2021-08-19 10:01:32 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!../usr/bin/bash
+pkg update upgrade -y
+pkg install ruby cowsay -y
+gem install lolcat
+echo "All dependencies have been installed, please run the command \"cowsay -f omii \"Replace with your text\" | lolcat\" to immediately run a script"
