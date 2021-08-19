b0159563d9c454ca590bbe1131b0d0c3c0960f90
HackD'mon <88218417+julyanto2@users.noreply.github.com>
2021-08-19 10:40:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!../usr/bin/bash
+cp omii.cow ../../../usr/share/cows
 pkg update upgrade -y
 pkg install ruby cowsay -y
 gem install lolcat
 echo "All dependencies have been installed, please run the command \"cowsay -f omii \"Replace with your text\" | lolcat\" to immediately run a script"
+exit
